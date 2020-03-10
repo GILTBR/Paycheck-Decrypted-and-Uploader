c594261779fbbccb5d73d358f437ea5ae88c8e68
Gil <giltober@gmail.com>
2020-03-10 17:23:33 +0200

    added docstrings and cleaned code

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# Paycheck Decrypted and Uploader
+### Paycheck Decrypted and Uploader
+Every month i get my paycheck in an encrypted PDF file.
+In order to make my life a bit easier and to follow the paychecks naming convention that I am used to, i created this program.
+
+
--- main.py
+++ main.py
@@ -11,7 +11,6 @@ import pdf_decrypter
 
 SCOPES = ['https://www.googleapis.com/auth/drive']
 CLIENT_SECRET_FILE = 'google_drive_api_credentials.json'  # Google Drive API credentials
-PARENT_FOLDER = json.load(open('config.json'))['Parent_Folder']  # The ID of the folder to which the file will be uploaded
 ID = json.load(open('config.json'))['ID']  # The ID to search for
 
 authIns = api_authentication.Auth(SCOPES, CLIENT_SECRET_FILE)
@@ -20,6 +19,10 @@ drive_service = build('drive', 'v3', credentials=credentials)
 
 
 def search_file():
+    """
+    This function searches in Google Drive for the last file that was uploaded that follows the convention of '{ID}_'.
+    After the file is found, the different elements from the file metadata are extracted and are passed on to the download_file function.
+    """
     results = drive_service.files().list(
         q=f"name contains '{ID}_.' and mimeType='application/pdf' and trashed=False", pageSize=1,
         orderBy='createdTime desc',
@@ -31,13 +34,19 @@ def search_file():
             file_id = item['id']
             file_name = item['name']
             file_parents = item['parents']
-            download_file(file_id, file_name)
+            download_file(file_id, file_name, file_parents)
     else:
         print('File not found, terminating program')
 
 
-def download_file(file_id, file_name):
-    """:arg file_id """
+def download_file(file_id, file_name, file_parents):
+    """
+    Downloads the file from Google Drive, calls the pdf_decrypter function and eventually calls the upload_file function.
+
+    :param file_id: the file id that was extracted from the metadata
+    :param file_name: the file name
+    :param file_parents: the id of the folder that the file is located in
+    """
     request = drive_service.files().get_media(fileId=file_id)
     fh = io.FileIO(file_name, 'wb')
     downloader = MediaIoBaseDownload(fh, request)
@@ -46,21 +55,32 @@ def download_file(file_id, file_name):
         status, done = downloader.next_chunk()
 
     file_to_upload = pdf_decrypter.decrypter(file_name)
-    upload_file(file_to_upload, file_id, PARENT_FOLDER)
+    upload_file(file_to_upload, file_id, file_parents)
 
 
 def upload_file(name, file_id, parents):
+    """
+    Uploads the new decrypted PDF file to the original file's folder in Google Drive.
+    Calls the delete_file function
+
+    :param name: the name of the new PDF file after decryption
+    :param file_id: the id of the original file
+    :param parents: the id of the folder that the original file is located in
+    """
     file_metadata = {'name': name, 'mimeType': 'application/pdf',
-                     'parents': PARENT_FOLDER}
+                     'parents': parents}
     media = MediaFileUpload(name, mimetype='application/pdf')
     drive_service.files().create(body=file_metadata,
                                  media_body=media,
                                  fields='id').execute()
-
     delete_file(file_id)
 
 
 def delete_file(file_id):
+    """
+    Deletes the original encrypted file from Google Drive
+    :param file_id: the id of the original file
+    """
     file_metadata = {'trashed': True}
     drive_service.files().update(fileId=file_id, body=file_metadata).execute()
     for f in glob.glob('*.pdf'):
